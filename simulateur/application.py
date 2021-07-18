a54004662c1f461a9b21320fdf12cbf8b0fe83db
dubicube <dubreuil.20100@gmail.com>
2021-07-18 12:59:50 +0200

    Improve robot simulation with screensaver style

--- simulateur/application.py
+++ simulateur/application.py
@@ -2,27 +2,29 @@ import math
 
 from config import *
 
+# Delta filter threshold
 DELTA_FILTER = 100
 
+# POI = Point Of Interest
 class POI:
     def __init__(self, i0, i1, average, size):
-        self.i0 = i0
-        self.i1 = i1
-        self.average = average
-        self.size = size
+        self.i0 = i0 # Discrete start angle
+        self.i1 = i1 # Discrete stop angle
+        self.average = average # Average distance of the points in the POI
+        self.size = size # Estimated size of the POI
     def get_all(self):
         return (self.i0, self.i1, self.average, self.size)
     def has_interesting_size(self):
         return self.size > 30 and self.size < 100
 
 
-
+# Calculate difference between each measured distance by the lidar
 def get_delta(distance):
     delta = [0]*len(distance)
     for i in range(len(distance)):
         delta[i] = distance[i]-distance[i-1]
     return delta
-
+# Filter with a threshold
 def filter_delta(delta):
     d = [0]*len(delta)
     for i in range(len(delta)):
@@ -36,7 +38,7 @@ def filter_delta(delta):
 def get_poi(lidar_data, delta):
     poi = []
     i_start = 0
-    # TODO: case where delta[i] always == 0
+    # TODO: case when delta[i] always == 0
     while delta[i_start] == 0:
         i_start+=1
     i0 = i_start
@@ -49,6 +51,9 @@ def get_poi(lidar_data, delta):
             # Remove POI with average bellow a certain value
             if average > 15:
                 # Estimate object size
+                # Don't know If should use a tan() or a sin(),
+                # but both produces not so good results
+                # This is possibly due to the distance/angle error
                 size = 2 * average * math.tan( ((i1-1-i0))*math.pi/LIDAR_POINTS_PER_TURN )
                 # Register POI
                 p = POI(i0, i1, average, size)
@@ -62,7 +67,8 @@ def get_poi(lidar_data, delta):
 
 # def detect_group(poi):
 
-
+# Main function
+# lidar_data is a list of distances measured by the lidar
 def solve_lidar(lidar_data):
     delta = get_delta(lidar_data)
     delta = filter_delta(delta)
--- simulateur/robot.py
+++ simulateur/robot.py
@@ -9,13 +9,15 @@ class Robot:
         self.y = y # Position Y
         self.r = r # Rotation R
 
-        self.dx = 10
-        self.dy = 0
+        self.dx = 50
+        self.dy = 50
 
         self.has_lidar = has_lidar
         self.lidar_data = None
 
         self.radius = 1200/(2*math.pi)
+
+        self.mode_xy = 1
     def get_center(self):
         return (self.x, self.y)
     def get_rotation(self):
@@ -27,10 +29,32 @@ class Robot:
 
         self.x+=self.dx
         self.y+=self.dy
-        if self.x < -1500+self.radius or self.x > 1500-self.radius:
-            self.dx = -self.dx
 
-        # self.r = (self.r+1)%360
+        if self.mode_xy == 0:
+            if self.x < -1500+self.radius and self.dx < 0:
+                self.dy = -self.dx
+                self.dx = 0
+            elif self.x > +1500-self.radius and self.dx > 0:
+                self.dy = -self.dx
+                self.dx = 0
+            elif self.y < -1000+self.radius and self.dy < 0:
+                self.dx = self.dy
+                self.dy = 0
+            elif self.y > +1000-self.radius and self.dy > 0:
+                self.dx = self.dy
+                self.dy = 0
+        elif self.mode_xy == 1:
+            if self.x < -1500+self.radius:
+                self.dx = -self.dx
+            elif self.x > +1500-self.radius:
+                self.dx = -self.dx
+            elif self.y < -1000+self.radius:
+                self.dy = -self.dy
+            elif self.y > +1000-self.radius:
+                self.dy = -self.dy
+
+
+        self.r = (self.r+10)%360
 
     def get_lidar_data(self):
         return self.lidar_data
